a5c75ff3e2d84bda483852b1c3f627b0f577ed20
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-07-13 12:27:49 +0900

    ArticleControllerの作成

--- src/main/java/com/bbs/controller/ArticleController.java
+++ src/main/java/com/bbs/controller/ArticleController.java
@@ -1,14 +1,31 @@
 package com.bbs.controller;
 
+import com.bbs.form.ArticleFrom;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class ArticleController {
 
-    @RequestMapping("")
+    @ModelAttribute
+    public ArticleFrom setUpArticleFrom(){
+        return new ArticleFrom();
+    }
+
+    @RequestMapping("show-list")
     public String index(){
         return "bbs";
+    }
+
+    /**
+     *
+     * 記事を投稿するコントローラーのメソッド
+     * @return
+     */
+    @RequestMapping("insert-article")
+    public String insertArticle(){
+        return index();
     }
 }
