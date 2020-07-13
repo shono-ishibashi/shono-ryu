375971a0e2d1d6c17525155017d71e488ed24308
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-07-13 11:00:38 +0900

    create project

---
+++ src/main/java/com/bbs/BbsTeamApplication.java
@@ -0,0 +1,13 @@
+package com.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsTeamApplication.class, args);
+    }
+
+}
