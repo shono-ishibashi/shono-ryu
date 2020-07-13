7136ed855a9fb3331425706e582662e79dac2a3d
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2020-07-13 12:18:41 +0900

    ArticleRepositoryを作成

--- src/main/java/com/bbs/repository/CommentRepository.java
+++ src/main/java/com/bbs/repository/CommentRepository.java
@@ -1,4 +1,28 @@
 package com.bbs.repository;
 
+import com.bbs.domain.Comment;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
 public class CommentRepository {
-}
+
+    private static final RowMapper<Comment> COMMENT_ROW_MAPPER = (rs, i) -> {
+
+        Comment comment = new Comment();
+
+        comment.setId(rs.getInt("id"));
+        comment.setName(rs.getString("name"));
+        comment.setContent(rs.getString("content"));
+
+        return comment;
+
+    };
+
+
+
+
+
+
