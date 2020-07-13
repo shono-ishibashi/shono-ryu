2d554d17c091a34946fc482b870e7b3272deec75
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-07-13 11:37:20 +0900

    replace domain

---
+++ src/main/java/com/bbs/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ArticleController {
+
+    @RequestMapping("")
+    public String index(){
+        return "bbs";
+    }
+}
